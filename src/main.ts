e1b5f878326de80d528d556163d4f08c8ef0639c
GoRbYsKkA <gleb.kovalchuk0609@gmail.com>
2024-02-15 13:47:34 +0200

    Add new feature

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { FileModule } from './file/file.module';
+
+async function bootstrap() {
+
+  const app = await NestFactory.create(FileModule);
+  await app.listen(3000);
+
+}
+
+bootstrap();
